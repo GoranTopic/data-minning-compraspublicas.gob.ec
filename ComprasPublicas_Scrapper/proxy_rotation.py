45c7fc5a4f98fd7dba8fd4d91445f6c152c453c6
Goran Topic <telegonicaxx@live.com>
2022-04-11 22:03:39 -0700

    added another proxies llist but unfrtunallty need to answer a capchan to get the url

--- ComprasPublicas_Scrapper/proxy_rotation.py
+++ ComprasPublicas_Scrapper/proxy_rotation.py
@@ -1,10 +1,23 @@
 import requests
 from lxml.html import fromstring
 
+
 def get_proxies(count=10):
-    url = 'https://free-proxy-list.net/'
     proxies = set()
-    while(len(proxies) < count):
+    #'https://free-proxy-list.net/'
+    proxies.update(from_free_proxy_list(count))
+    if(len(proxies) < count):
+        #'https://advanced.name/freeproxy'
+        proxies.update(advanced_free_proxy())
+    return proxies
+
+def from_free_proxy_list(count):
+    # this website provides about 21 free proxies
+    url = 'https://free-proxy-list.net/'
+    max_proxies = 21
+    limit = max_proxies if count > proxies else count
+    proxies = set()
+    while(len(proxies) < limit):
         response = requests.get(url)
         parser = fromstring(response.text)
         print(f"grabbing proxies: {len(proxies)} out of {count}");
@@ -15,5 +28,16 @@ def get_proxies(count=10):
                 proxies.add(proxy)
     return proxies
 
-#proxies = get_proxies()
-#print(proxies)
+def advanced_free_proxy():
+    url = 'https://advanced.name/freeproxy/6255013b47ba6'
+    response = requests.get(url)
+    proxies = set()
+    # = fromstring(response.text)
+    print(type(response.text))
+    for line in response.text.splitlines():
+        proxies.add(line)
+    return proxies
+
+
+proxies = get_proxies(100)
+print(proxies)
